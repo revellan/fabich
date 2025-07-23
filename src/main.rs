be9eac7106f4a8d355587f845897570eee48f50b
revellan <climb.simonschmidt@web.de>
2025-07-23 08:15:24 +0200

    Add christmas carol function

--- src/main.rs
+++ src/main.rs
@@ -160,4 +160,30 @@ fn fibonacci() {
     };
     println!("{}", out);
 }
-fn christmas_carol() {}
+fn christmas_carol() {
+    const DAYS: usize = 12;
+    let days: [&str; DAYS] = [
+        "first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth",
+        "tenth", "eleventh", "twelfth",
+    ];
+    let lines: [&str; DAYS] = [
+        "Twelve drummers drumming",
+        "Eleven pipers piping",
+        "Ten lords a-leaping",
+        "Nine ladies dancing",
+        "Eight maids a-milking",
+        "Seven swans a-swimming",
+        "Six geese a-laying",
+        "Five golden rings",
+        "Four calling birds",
+        "Three french hens",
+        "Two turtle doves and",
+        "A partridge in a pear tree",
+    ];
+    for i in 0..DAYS {
+        println!("\nOn the {} day of Christmas, my true love sent to me", days[i]);
+        for n in DAYS - (i + 1)..DAYS {
+            println!("{}", lines[n]);
+        }
+    }
+}
